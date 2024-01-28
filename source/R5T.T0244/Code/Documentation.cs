b5dbd5713fbaf24a8981645a9be22431b92e9415
davidcoats <david@getsafetycone.com>
2024-01-27 22:23:43 -0800

    Initial commit.

---
+++ source/R5T.T0244/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0244
+{
+	/// <summary>
+	/// Check marker attribute and marker interface types library.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
